ccb21db023284b2fa0fbfcc7a6e8e5f6b77f244f
DESKTOP-R6M0T7Q\Paulo Rodrigues <pauloroberto.prrl@gmail.com>
2017-11-10 01:18:55 -0200

    Integração ao Firebase e cadastro de usuário.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
@@ -12,6 +16,17 @@ import { PhotosPage } from '../pages/photos/photos';
 import { TakePicturePage } from '../pages/take-picture/take-picture';
 import { SendPhotoPage } from '../pages/send-photo/send-photo';
 import { ProfilePage } from '../pages/profile/profile';
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDMImZeA_FTotxEvcuNAJnD6SgC0yo4u2E",
+    authDomain: "palmagram.firebaseapp.com",
+    databaseURL: "https://palmagram.firebaseio.com",
+    projectId: "palmagram",
+    storageBucket: "palmagram.appspot.com",
+    messagingSenderId: "262937886302"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -26,7 +41,11 @@ import { ProfilePage } from '../pages/profile/profile';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
