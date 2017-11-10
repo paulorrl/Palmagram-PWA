0b9dd0ef9c3daae18d19fa02be423b3eb1979715
DESKTOP-R6M0T7Q\Paulo Rodrigues <pauloroberto.prrl@gmail.com>
2017-11-10 13:32:45 -0200

    Funcionalidades implementadas: Login, profile (logout) e manter usuário logado.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,65 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController, AlertController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { SignupPage } from '../signup/signup';
+import { HomePage } from '../home/home';
 
 @Component({
     selector: 'page-login',
     templateUrl: 'login.html'
 })
 export class LoginPage {
-    constructor(public navCtrl: NavController) {
+    public form: FormGroup;
+
+    constructor(
+        private fb: FormBuilder,
+        private afAuth: AngularFireAuth,
+        private loadingCtrl: LoadingController,
+        private navCtrl: NavController,
+        private alertCtrl: AlertController) {
+            this.form = this.fb.group({
+                email: ['', Validators.compose([
+                    Validators.minLength(5),
+                    Validators.maxLength(140),
+                    Validators.required
+                ])],
+                password: ['', Validators.compose([
+                    Validators.minLength(6),
+                    Validators.maxLength(20),
+                    Validators.required
+                ])]
+            });
+
+            this.afAuth.authState.subscribe(user => {
+                if (user) {
+                    this.navCtrl.setRoot(HomePage);
+                }
+            });
+    }
+
+    goToSignup() {
+        this.navCtrl.setRoot(SignupPage);
+    }
+
+    submit() {
+        let loader = this.loadingCtrl.create({ content: 'Autenticando...' });
+        loader.present();
+
+        this.afAuth.auth
+            .signInWithEmailAndPassword(this.form.controls['email'].value, this.form.controls['password'].value)
+            .then(() => {
+                loader.dismiss();
+                this.navCtrl.setRoot(HomePage);
+            })
+            .catch(() => {
+                loader.dismiss();
+                this.alertCtrl.create({
+                    title: 'Autenticação inválida',
+                    subTitle: 'Usuário ou senha incorretos',
+                    buttons: ['OK']
+                })
+                .present();
+            });
     }
 }
